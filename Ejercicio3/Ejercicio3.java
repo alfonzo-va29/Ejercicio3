dd4fd4af685859b95ecb69a67db9974c1c969666
alfonzo-va29 <81872583+alfonzo-va29@users.noreply.github.com>
2021-06-23 11:42:11 -0400

    Add files via upload

---
+++ Ejercicio3/Ejercicio3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Ejercicio3{
+    public static void main (String[] args){
+        Scanner add = new Scanner(System.in);
+        
+        System.out.println("Ingresa un Nombre");        
+        String nombre = add.next();
+        
+        
+        System.out.println("Ingresa un Apellido");
+        String apellido = add.next();
+        
+        System.out.println("--------------------------------");
+        System.out.println("Hoola mi nombre es : "+ nombre);
+        System.out.println("Mi apellido es : "+ apellido);
+        
+        
+    }
+}
+
